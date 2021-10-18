0a5632a764301683eb08ca6df10f76d64eff3cf9
Weii <58661923+Weisplendid@users.noreply.github.com>
2021-10-18 17:26:03 +0800

    Update Oral_calculation_exercise.java

--- Oral_calculation_exercise.java
+++ Oral_calculation_exercise.java
@@ -88,4 +88,43 @@ public class Oral_calculation_exercise {
 				break;
 			}
 	}
+	public static boolean Cal_MD(){//输入1是乘法，0是除法
+		int i = new java.util.Random().nextBoolean() ? 1 : 0;
+		boolean output = false;
+		boolean test = true;
+		Scanner sc=new Scanner(System.in);
+		
+		if(i == 1){
+			int num1 = (int)(Math.random()*100);//乘数1
+			int num2 = (int)(Math.random()*100);//乘数2
+			System.out.println(num1 +" * "+ num2 +" = ?");
+			int num3 = num1*num2;//乘积
+			int c=sc.nextInt();
+			if(c == num3){
+				output = true;
+			} else {
+				output = false;
+			}
+		}
+		
+		if(i == 0){
+			while(test == true){
+				int num5 = (int)(Math.random()*99 + 1);//被除数
+				int num6 = (int)(Math.random()*99 + 1);//商				
+				int num4 = num5*num6;//除数				
+				if(num4 < 100){
+					System.out.println(num4 +" / "+ num5 +" = ?");
+					int c=sc.nextInt();
+					if(c == num6){
+						output = true;
+					} else {
+						output = false;
+					}
+					test = false;
+				}
+			}
+		}
+		
+		return output;
+	}
 }
