6c192350f69d39f513878a42cc34ade066dc7778
Weii <58661923+Weisplendid@users.noreply.github.com>
2021-10-18 17:20:41 +0800

    Update Oral_calculation_exercise.java

--- Oral_calculation_exercise.java
+++ Oral_calculation_exercise.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 import org.omg.CORBA.PUBLIC_MEMBER;
 
@@ -29,7 +29,7 @@ public class Oral_calculation_exercise {
 		System.out.println("Please enter the number of questions?");
 		questionNumber = sc.nextInt();
 		while(questionNumber-- > 0) {
-//			select(grade);
+			select(grade);
 			System.out.println("yes");
 		}
 		System.out.print("end!");
@@ -39,5 +39,53 @@ public class Oral_calculation_exercise {
 		}else if(score == 100) {
 			System.out.print("All right, that's great! your score is " + score + ".");
 		}
+	}
+	public static void select(int gradenum) {
+
+		if(gradenum==1)
+			countgrade(1);
+		if(gradenum==2)
+			if(getRandomNumberInRange(0,9)<3)
+				countgrade(1);
+			else
+				countgrade(2);
+		if(gradenum==3)
+			{
+			int temp=getRandomNumberInRange(0,9);
+				if(temp==0)
+					countgrade(1);
+				else if(temp<4)
+					countgrade(2);
+				else 
+					countgrade(3);
+
+
+			}
+	}
+	public static int getRandomNumberInRange(int min, int max) {
+
+		if (min >= max) {
+			throw new IllegalArgumentException("max must be greater than min");
+		}
+
+		Random r = new Random();
+		return r.nextInt((max - min) + 1) + min;
+	}
+	public static void countgrade(int gradenum) {
+		switch (gradenum)
+			{
+			case 1:
+				if(Grade12Method())
+					correctNumber++;
+				break;
+			case 2:
+				if(Cal_MD())
+					correctNumber++;
+				break;
+			case 3:
+				if(float_cal())
+					correctNumber++;
+				break;
+			}
 	}
 }
